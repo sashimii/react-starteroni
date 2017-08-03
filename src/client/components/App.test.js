8b855ab442827f4224be639bb00e174823d7f852
Sushil Tailor <sushil.tailor@gmail.com>
2017-08-03 11:52:32 -0400

    Testing ESlint and Sass-lint

--- src/client/components/App.test.js
+++ src/client/components/App.test.js
@@ -1,15 +1,5 @@
 import test from 'ava';
 
-import React from 'react';
-// import {shallow} from 'enzyme';
-// import App from './App.react';
-
-test('foo', t => {
-	t.fail();
+test('foo', (t ) => {
+  t.pass();
 });
-//
-// test('bar', async t => {
-// 	const bar = Promise.resolve('bar');
-//
-// 	t.is(await bar, 'bar');
-// });
